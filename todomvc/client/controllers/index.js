8b737da31895e08f4535357327d6f8aa2792c25c
Luis Fernando Barrera <luisfernando@informind.com>
2016-07-25 13:44:24 -0500

    ControllerFactory

--- todomvc/client/controllers/index.js
+++ todomvc/client/controllers/index.js
@@ -1,6 +1,6 @@
 import ControllerFactory from './ControllerFactory'
 
-let TodoController = new ControllerFactory('todos', '/api/todos/');
+let TodoController = new ControllerFactory('todos', '/api/todos');
 
 
 export default TodoController;
--- todomvc/server/todomvc/routers.py
+++ todomvc/server/todomvc/routers.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 
 
 class SharedAPIRootRouter():
-    router = routers.DefaultRouter()
+    router = routers.DefaultRouter(trailing_slash=False)
 
     def register(self, *args, **kwargs):
         self.router.register(*args, **kwargs)
